b2c82d01cb1ee8e4d6e83ff964777a75acd51589
mt12777 <marieta.080@mail.ru>
2025-06-18 20:27:33 +0400

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,6 +1,6 @@
 import os
 from aiogram import Bot, Dispatcher, types
-from aiogram.webhook.aiohttp_server import setup_application
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from aiohttp import web
 
 TOKEN = os.getenv("BOT_TOKEN")
